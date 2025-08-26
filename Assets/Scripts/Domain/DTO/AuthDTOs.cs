21e1c8596c7bed8a21fc316962dbe0bedc3ec414
Hot6Mania <than7683@gmail.com>
2025-08-27 02:16:03 +0900

    add DTO in Domain namespace

---
+++ Assets/Scripts/Domain/DTO/AuthDTOs.cs
@@ -0,0 +1,40 @@
+namespace SCOdyssey.Domain.Dto
+{
+    [Serializable] public class AuthTokens
+    {
+        public string access_token;
+        public string refresh_token;
+        public int    expires_in;   // sec
+        public string token_type;   // "bearer"
+        public bool   is_new_user;
+    }
+
+    [Serializable] public class UserMe
+    {
+        public string id;
+        public string uid;
+        public string nickname;
+        public string created_at;
+        public string updated_at;
+        public string last_login_at;
+    }
+
+    [Serializable] public class LinkOut
+    {
+        public string provider;
+        public string provider_sub;
+    }
+
+    [Serializable] public class UnlinkOut
+    {
+        public bool deleted;
+        public string provider;
+        public string provider_sub;
+    }
+
+    [Serializable] public class ApiError
+    {
+        public string detail;
+        public string code;
+    }
+}
---
+++ Assets/Scripts/Domain/DTO/GameDTOs.cs
@@ -0,0 +1,22 @@
+namespace SCOdyssey.Domain.Dto
+{
+    [Serializable]
+    public class SessionStartReq
+    {
+        public string chart_id;
+        public string seed;
+    }
+    [Serializable]
+    public class GameSession
+    {
+        public string session_id;
+        public long issued_at;
+    }
+    [Serializable]
+    public class SessionResultReq
+    {
+        public string session_id;
+        public int score;
+        public string hash;
+    }
+}
